8e693ffa5cb6438ae89a3c23ac88d920c2779189
swapnil <sutarswapnil322@gmail.com>
2024-10-07 21:23:36 +0530

    create helper file to upload images

---
+++ Frontend_D-voting/src/utils/UploadImage.jsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+const apiurl = import.meta.env.VITE_WEB_URL;
+
+const UploadImage = async (file, storagePath) => {
+  try {
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const token = localStorage.getItem("token");
+
+    const config = {
+      headers: {
+        "x-access-token": token,
+        "Content-Type": "multipart/form-data",
+      },
+    };
+
+    const res = await axios.post(
+      `${apiurl}/api/v1/${storagePath}`,
+      formData,
+      config
+    );
+
+    return res.data.imageUrl;
+  } catch (error) {
+    console.error("Error occurred during image upload:", error);
+    throw error;
+  }
+};
+
+export { UploadImage };
