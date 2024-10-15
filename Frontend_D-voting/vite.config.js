6b6d8e7479fba640431259bdb5b1216793866f60
swapnil <sutarswapnil322@gmail.com>
2024-10-15 19:13:49 +0530

    add vercel config

--- Frontend_D-voting/vite.config.js
+++ Frontend_D-voting/vite.config.js
@@ -1,12 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   server: {
     host: true,
-    port: 5173, 
+    port: 5173,
   },
-});
+  build: {
+    outDir: "dist",
+  },
 
+  resolve: {
+    alias: {},
+  },
+  preview: {
+    port: 4173,
+  },
+  plugins: [vercel()],
+});
