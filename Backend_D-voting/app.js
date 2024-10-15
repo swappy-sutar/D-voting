5eb4dca1d1aacbc3edd2889fb6c0db6804751c5e
swapnil <sutarswapnil322@gmail.com>
2024-10-15 19:50:48 +0530

    add home route

--- Backend_D-voting/app.js
+++ Backend_D-voting/app.js
@@ -9,22 +9,31 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(
-  cors({
-    origin: ["https://d-voting-v1.vercel.app"],
-    methods: ["POST", "GET", "DELETE"],
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: [
+    "https://d-voting-v1.vercel.app",
+    "https://d-voting-omdk.vercel.app",
+  ],
+  methods: ["POST", "GET", "DELETE", "OPTIONS"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
+
 app.use("/D-voting", express.static(path.join(__dirname, "D-voting")));
 
+// Define routes
 import { router as candidate } from "./routes/candidate.Routes.js";
 import { router as voter } from "./routes/voter.routes.js";
 import { router as Authentication } from "./routes/auth.routes.js";
 import { home } from "./controllers/home.controller.js";
 
-// app.use("/", home);
+app.options("*", cors()); 
+
+// Use routes
+app.use("/",home);
 app.use("/api/v1/auth", Authentication);
 app.use("/api/v1/candidate", auth, candidate);
 app.use("/api/v1/voter", auth, voter);
--- Backend_D-voting/controllers/home.controller.js
+++ Backend_D-voting/controllers/home.controller.js
@@ -1,6 +1,47 @@
 const home = async (req, res) => {
   try {
-    return res.send("<h1>D-voting </h1>"); 
+    const htmlResponse = `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>D-Voting Backend</title>
+        <style>
+          body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            color: #333;
+          }
+          h1 {
+            font-size: 3rem;
+            color: #4CAF50; /* Green color */
+            text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1);
+          }
+          .container {
+            text-align: center;
+            padding: 20px;
+            border: 2px solid #4CAF50; /* Green border */
+            border-radius: 10px;
+            background-color: white;
+            box-shadow: 0 4px 20px rgba(0, 0, 0, 0.1);
+          }
+        </style>
+      </head>
+      <body>
+        <div class="container">
+          <h1>Wel-come to D-Voting Backend</h1>
+        </div>
+      </body>
+      </html>
+    `;
+
+    return res.send(htmlResponse);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
