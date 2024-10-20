c6bcb0941c8a6d22622be600b033250a1e6501c5
swapnil <sutarswapnil322@gmail.com>
2024-10-20 14:19:05 +0530

    update the code

--- Frontend_D-voting/src/utils/HandleAccountChange.jsx
+++ Frontend_D-voting/src/utils/HandleAccountChange.jsx
@@ -1,10 +1,18 @@
 const handleAccountChange = async (setWeb3State) => {
-  const accounts = await window.ethereum.request({
-    method: "eth_requestAccounts",
-  });
-  const selectedAccount = accounts[0];
+  if (typeof window.ethereum !== "undefined") {
+    try {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const selectedAccount = accounts[0];
 
-  setWeb3State((prevState) => ({ ...prevState, selectedAccount }));
+      setWeb3State((prevState) => ({ ...prevState, selectedAccount }));
+    } catch (error) {
+      console.error("Error fetching accounts", error);
+    }
+  } else {
+    console.error("MetaMask is not installed or not detected");
+  }
 };
 
 export { handleAccountChange };
