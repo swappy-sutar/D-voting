3eb61911d52f90c482c00fe6d59d2e1cfa1e0515
swapnil <sutarswapnil322@gmail.com>
2024-10-20 13:24:34 +0530

    debug

--- Frontend_D-voting/src/Components/Wallet/Wallet.jsx
+++ Frontend_D-voting/src/Components/Wallet/Wallet.jsx
@@ -8,61 +8,64 @@ import Layout from "../Layout";
 import { motion } from "framer-motion";
 
 const Wallet = () => {
-  const { handleWallet, web3State } = useWeb3Context();
-  const { selectedAccount } = web3State;
-  const [loading, setLoading] = useState(false);
-  const navigateTo = useNavigate();
+  // const { handleWallet, web3State } = useWeb3Context();
+  // const { selectedAccount } = web3State;
+  // const [loading, setLoading] = useState(false);
+  // const navigateTo = useNavigate();
 
-  useEffect(() => {
-    if (selectedAccount) {
-      setLoading(false);
-      toast.success(`Wallet connected: ${selectedAccount.slice(0, 6)}...`);
-      navigateTo("/select");
-    }
-  }, [selectedAccount, navigateTo]);
+  // useEffect(() => {
+  //   if (selectedAccount) {
+  //     setLoading(false);
+  //     toast.success(`Wallet connected: ${selectedAccount.slice(0, 6)}...`);
+  //     navigateTo("/select");
+  //   }
+  // }, [selectedAccount, navigateTo]);
 
-  const handleConnectWallet = async () => {
-    setLoading(true); 
-    await handleWallet();
-  };
+  // const handleConnectWallet = async () => {
+  //   setLoading(true); 
+  //   await handleWallet();
+  // };
 
-  return (
-    <Layout>
-      <div className="relative flex flex-col justify-center items-center h-screen px-4 md:px-8 lg:px-16">
-        <ToastContainer />
-        <div className="relative z-10 flex flex-col items-center text-center animate-fadeIn">
-          <img
-            className="w-32 h-32 md:w-48 md:h-48 mb-6 drop-shadow-2xl rounded-full shadow-2xl transform transition-transform duration-300 hover:scale-110 animate-zoomIn"
-            src={voterPersonImg}
-            alt="Voter"
-          />
+  return(
+    <h1>debug</h1>
+  )
+//   return (
+//     <Layout>
+//       <div className="relative flex flex-col justify-center items-center h-screen px-4 md:px-8 lg:px-16">
+//         <ToastContainer />
+//         <div className="relative z-10 flex flex-col items-center text-center animate-fadeIn">
+//           <img
+//             className="w-32 h-32 md:w-48 md:h-48 mb-6 drop-shadow-2xl rounded-full shadow-2xl transform transition-transform duration-300 hover:scale-110 animate-zoomIn"
+//             src={voterPersonImg}
+//             alt="Voter"
+//           />
 
-          <h1 className="text-white bg-black text-4xl p-4 rounded-lg md:text-7xl font-bold mb-8 drop-shadow-lg">
-            Welcome to D-Voting
-            <motion.p
-              className="text-white text-base p-2 rounded-lg md:text-lg font-bold mb-4 drop-shadow-lg"
-              initial={{ scale: 0 }}
-              animate={{ scale: 1 }}
-              transition={{ duration: 1.5, delay: 0.2 }}
-            >
-              "Decentralized Voting: The Future of Elections"
-            </motion.p>
-          </h1>
+//           <h1 className="text-white bg-black text-4xl p-4 rounded-lg md:text-7xl font-bold mb-8 drop-shadow-lg">
+//             Welcome to D-Voting
+//             <motion.p
+//               className="text-white text-base p-2 rounded-lg md:text-lg font-bold mb-4 drop-shadow-lg"
+//               initial={{ scale: 0 }}
+//               animate={{ scale: 1 }}
+//               transition={{ duration: 1.5, delay: 0.2 }}
+//             >
+//               "Decentralized Voting: The Future of Elections"
+//             </motion.p>
+//           </h1>
 
-          <button
-            onClick={handleConnectWallet}
-            className="px-8 py-4 bg-white text-indigo-600 font-bold text-lg rounded-full shadow-lg transform hover:scale-110 transition duration-300 ease-in-out hover:bg-indigo-100 hover:text-black focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:ring-opacity-50 animate-bounce"
-          >
-            {loading
-              ? "Connecting..." 
-              : selectedAccount
-              ? `Connected: ${selectedAccount.slice(0, 6)}...`
-              : "Connect Wallet"}
-          </button>
-        </div>
-      </div>
-    </Layout>
-  );
+//           <button
+//             onClick={handleConnectWallet}
+//             className="px-8 py-4 bg-white text-indigo-600 font-bold text-lg rounded-full shadow-lg transform hover:scale-110 transition duration-300 ease-in-out hover:bg-indigo-100 hover:text-black focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:ring-opacity-50 animate-bounce"
+//           >
+//             {loading
+//               ? "Connecting..." 
+//               : selectedAccount
+//               ? `Connected: ${selectedAccount.slice(0, 6)}...`
+//               : "Connect Wallet"}
+//           </button>
+//         </div>
+//       </div>
+//     </Layout>
+//   );
 };
 
 export default Wallet;
