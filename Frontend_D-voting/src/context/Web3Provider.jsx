d28508f904d161fe78618b12553da3ed9b00030f
swapnil <sutarswapnil322@gmail.com>
2024-10-20 16:03:08 +0530

    handle metamask for android

--- Frontend_D-voting/src/context/Web3Provider.jsx
+++ Frontend_D-voting/src/context/Web3Provider.jsx
@@ -4,8 +4,8 @@ import { GetWeb3State } from "../utils/GetWeb3State";
 import { handleAccountChange } from "../utils/HandleAccountChange";
 import { handleChainChange } from "../utils/HandleChainChange";
 import { toast } from "react-toastify";
-import { motion } from "framer-motion";
 import Layout from "../Components/Layout";
+import { motion } from "framer-motion";
 
 const Web3Provider = ({ children }) => {
   const [web3State, setWeb3State] = useState({
@@ -18,21 +18,30 @@ const Web3Provider = ({ children }) => {
 
   const handleWallet = async () => {
     try {
-      const { contractInstance, selectedAccount, chainId } =
-        await GetWeb3State();
-      setWeb3State({ contractInstance, selectedAccount, chainId });
+      if (window.ethereum) {
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+
+        const { contractInstance, selectedAccount, chainId } =
+          await GetWeb3State();
+        setWeb3State({ contractInstance, selectedAccount, chainId });
+      } else {
+        setHasMetaMask(false);
+        toast.error(
+          "MetaMask is not detected! Please install MetaMask to use this application."
+        );
+      }
     } catch (error) {
-      console.error(
-        "contractInstance, selectedAccount, chainId is not found",
-        error
-      );
+      console.error("Error connecting to wallet:", error);
+      toast.error("Failed to connect to wallet.");
     }
   };
 
   useEffect(() => {
     if (typeof window.ethereum === "undefined") {
       setHasMetaMask(false);
-      toast.error("Please install MetaMask");
+      toast.error(
+        "MetaMask is not detected! Please install MetaMask to use this application."
+      );
     } else {
       window.ethereum.on("accountsChanged", () =>
         handleAccountChange(setWeb3State)
@@ -50,31 +59,38 @@ const Web3Provider = ({ children }) => {
     }
   }, []);
 
- 
   if (!hasMetaMask) {
     return (
       <Layout>
         <div className="relative flex flex-col items-center justify-center h-screen px-4 md:px-8 lg:px-16">
-          <div className="relative z-10 flex flex-col items-center text-center ">
-            <h1 className="text-white bg-black text-4xl p-4 rounded-lg md:text-7xl font-bold mb-8 drop-shadow-lg">
+          <div className="relative z-10 flex flex-col items-center text-center animate-fadeIn">
+            <motion.h1
+              className="text-white bg-black text-xl md:text-6xl p-6 rounded-lg font-bold mb-5 drop-shadow-lg"
+              initial={{ opacity: 0, y: -50 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1 }}
+            >
               MetaMask Not Detected
               <motion.p
-                className="text-white text-base p-2 rounded-lg md:text-lg font-bold mb-4 drop-shadow-lg"
-                initial={{ scale: 0 }}
-                animate={{ scale: 1 }}
-                transition={{ duration: 1.5, delay: 0.2 }}
+                className="text-white text-lg md:text-xl m-5"
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 1.5 }}
               >
-                "Please install MetaMask Wallet to use this application"
+                "Please install MetaMask to use this application"
               </motion.p>
-            </h1>
+            </motion.h1>
 
             <motion.button
               onClick={() =>
                 window.open("https://metamask.io/download.html", "_blank")
               }
-              className="px-8 py-4 bg-white text-indigo-600 font-bold text-lg rounded-full shadow-lg transform hover:scale-110 transition duration-300 ease-in-out hover:bg-indigo-100 hover:text-black focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:ring-opacity-50 animate-bounce"
+              className="px-8 py-4 bg-white text-indigo-600 font-bold text-lg rounded-full shadow-lg transform hover:scale-110 transition duration-300 ease-in-out hover:bg-indigo-100 hover:text-black focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:ring-opacity-50"
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1 }}
             >
-              Install MetaMask🦊
+              Install MetaMask
             </motion.button>
           </div>
         </div>
