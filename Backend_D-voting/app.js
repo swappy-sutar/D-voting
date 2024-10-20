5de80d6cb579febf26116d314415cec5c366b03f
swapnil <sutarswapnil322@gmail.com>
2024-10-20 12:42:02 +0530

    remove unused things currect typo in election-Commission.routes.js

--- Backend_D-voting/app.js
+++ Backend_D-voting/app.js
@@ -1,40 +1,27 @@
 import express from "express";
-import path from "path"; // Import path
-import { fileURLToPath } from "url";
 import cors from "cors";
-import { auth } from "./middlewares/auth.middleware.js";
 
 const app = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const corsOptions = {
-  origin: [
-    "https://d-voting-swappy.vercel.app"
-  ],
+  origin: ["http://localhost:5173", "https://d-voting-swappy.vercel.app"],
   methods: ["POST", "GET", "DELETE", "OPTIONS"],
   credentials: true,
 };
 
-
 app.use(cors(corsOptions));
-
-
 app.use(express.json());
+app.options("*", cors());
 
-app.use("/D-voting", express.static(path.join(__dirname, "D-voting")));
-
-
+import { auth } from "./middlewares/auth.middleware.js";
 import { router as candidate } from "./routes/candidate.Routes.js";
 import { router as voter } from "./routes/voter.routes.js";
 import { router as Authentication } from "./routes/auth.routes.js";
-
-app.options("*", cors());
-
+import { router as electionCommission } from "./routes/election-Commission.routes.js";
 
 app.use("/api/v1/auth", Authentication);
 app.use("/api/v1/candidate", auth, candidate);
 app.use("/api/v1/voter", auth, voter);
+app.use("/api/v1/election-Commission", auth, electionCommission);
 
 export { app };
