538ae9c64b7534930598a6ccf121d602fdb76837
swapnil <sutarswapnil322@gmail.com>
2024-10-20 16:50:27 +0530

    add useEffect

--- Frontend_D-voting/src/Components/admin/Login.jsx
+++ Frontend_D-voting/src/Components/admin/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef,useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
