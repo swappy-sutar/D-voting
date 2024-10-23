075876ce99948fa924a099e9d9d0686fd3d4d746
swapnil <sutarswapnil322@gmail.com>
2024-10-23 17:59:15 +0530

    update firebaseConfig

--- Frontend_D-voting/src/firebase.js
+++ Frontend_D-voting/src/firebase.js
@@ -3,12 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API_KEY,
-  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyByIuA_uC2oGWYC9QToL6ke7RvbVF8sTsg",
+  authDomain: "d-voting-e88f0.firebaseapp.com",
+  projectId: "d-voting-e88f0",
+  storageBucket: "d-voting-e88f0.appspot.com",
+  messagingSenderId: "743431811684",
+  appId: "1:743431811684:web:71fceb8fe6cf6979a6e571",
+  measurementId: "G-0M3YYFSH27"
 };
 
 const app = initializeApp(firebaseConfig);
