206ff05c23488adcc25845f5a55d50712f19114d
swapnil <sutarswapnil322@gmail.com>
2024-10-29 00:29:12 +0530

    update css

--- Frontend_D-voting/src/pages/Winner.jsx
+++ Frontend_D-voting/src/pages/Winner.jsx
@@ -17,9 +17,9 @@ function Winner() {
     try {
       const winnerAddress = await contractInstance.winner();
 
-      if (winnerAddress == 0x0000000000000000000000000000000000000000) {
-        toast.error("Winner is not declare yet!");
-      }else{
+      if (winnerAddress === "0x0000000000000000000000000000000000000000") {
+        toast.error("Winner is not declared yet!");
+      } else {
         const candidate = await contractInstance.getCandidate(winnerAddress);
         setWinnerInfo({
           address: winnerAddress,
@@ -27,9 +27,7 @@ function Winner() {
           party: candidate[1],
         });
         setShowConfetti(true);
-
       }
-
     } catch (err) {
       console.error("Error fetching winner:", err);
       toast.error("Winner is not declared.");
@@ -40,12 +38,12 @@ function Winner() {
 
   return (
     <Layout>
-      <div className="flex flex-col items-center justify-center min-h-screen p-4 ">
+      <div className="flex flex-col items-center justify-center min-h-screen p-4">
         <ToastContainer />
 
         <div className="w-full max-w-3xl bg-white p-8 rounded-xl shadow-lg border border-gray-300 relative">
           {showConfetti && (
-            <Confetti className=" justify-center  h-full w-full" />
+            <Confetti className="justify-center h-full w-full" />
           )}
           <h1 className="text-5xl font-extrabold text-blue-900 mb-6 text-center animate-bounce">
             Winner Details
@@ -59,7 +57,7 @@ function Winner() {
                 className={`px-8 py-4 w-full md:w-auto text-xl font-bold rounded-lg shadow-lg ${
                   loading
                     ? "bg-gray-400 cursor-not-allowed"
-                    : "bg-gradient-to-r from-green-500 to-blue-600 "
+                    : "bg-gradient-to-r from-green-500 to-blue-600"
                 } text-white mb-6 mx-auto`}
               >
                 {loading ? (
@@ -75,7 +73,7 @@ function Winner() {
           )}
 
           {winnerInfo && (
-            <div className="bg-white p-6 rounded-xl shadow-2xl border-black w-full max-w-lg mx-auto transform transition-all duration-300 ease-in-out">
+            <div className="bg-white p-6 rounded-xl shadow-2xl border border-gray-300 w-full max-w-lg mx-auto transform transition-all duration-300 ease-in-out">
               <div className="text-center mb-4 animate-fade-in">
                 <h2 className="text-2xl font-semibold text-gray-800 mb-2">
                   🎉 Congratulations! 🎉
