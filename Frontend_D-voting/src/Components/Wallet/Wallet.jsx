6a1003c836e9696fdaff9c40e68aae3d721c4c5e
swapnil <sutarswapnil322@gmail.com>
2024-10-29 00:26:49 +0530

    add Created By: Er.Swappy❤️

--- Frontend_D-voting/src/Components/Wallet/Wallet.jsx
+++ Frontend_D-voting/src/Components/Wallet/Wallet.jsx
@@ -12,7 +12,6 @@ const Wallet = () => {
   const { selectedAccount } = web3State;
   const [loading, setLoading] = useState(false);
   const navigateTo = useNavigate();
-  
 
   useEffect(() => {
     if (selectedAccount) {
@@ -23,7 +22,7 @@ const Wallet = () => {
   }, [selectedAccount, navigateTo]);
 
   const handleConnectWallet = async () => {
-    setLoading(true); 
+    setLoading(true);
     await handleWallet();
   };
 
@@ -33,29 +32,36 @@ const Wallet = () => {
         <ToastContainer />
         <div className="relative z-10 flex flex-col items-center text-center animate-fadeIn">
           <img
-            className="w-32 h-32 md:w-48 md:h-48 mb-6 drop-shadow-2xl rounded-full shadow-2xl transform transition-transform duration-300 hover:scale-110 animate-zoomIn"
+            className="w-32 h-32 md:w-48 md:h-48 mb-6 drop-shadow-2xl rounded-full transform transition-transform duration-300 hover:scale-110 animate-zoomIn"
             src={voterPersonImg}
             alt="Voter"
           />
 
-          <h1 className="text-white bg-black text-4xl p-4 rounded-lg md:text-7xl font-bold mb-8 drop-shadow-lg">
+          <h1 className="text-white bg-black text-4xl p-4 md:text-7xl font-bold mb-8 drop-shadow-lg border-2 rounded-lg border-sky-200 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f] transition duration-300 ">
             Welcome to D-Voting
             <motion.p
-              className="text-white text-base p-2 rounded-lg md:text-lg font-bold mb-4 drop-shadow-lg"
+              className="text-white text-base p-2 rounded-lg md:text-lg font-bold drop-shadow-lg animate-pulse"
               initial={{ scale: 0 }}
               animate={{ scale: 1 }}
               transition={{ duration: 1.5, delay: 0.2 }}
             >
               "Decentralized Voting: The Future of Elections"
             </motion.p>
+            <a
+              href="https://er-swappy.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-yellow-300 transition duration-300 text-lg "
+            >
+              Created By: Er.Swappy❤️
+            </a>
           </h1>
-
           <button
             onClick={handleConnectWallet}
             className="px-8 py-4 bg-white text-indigo-600 font-bold text-lg rounded-full shadow-lg transform hover:scale-110 transition duration-300 ease-in-out hover:bg-indigo-100 hover:text-black focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:ring-opacity-50 animate-bounce"
           >
             {loading
-              ? "Connecting..." 
+              ? "Connecting..."
               : selectedAccount
               ? `Connected: ${selectedAccount.slice(0, 6)}...`
               : "Connect Wallet"}
